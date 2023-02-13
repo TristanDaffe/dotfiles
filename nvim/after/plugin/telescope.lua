24fd61a7cee44f8bd7d3bd0ba50c49494a58873c
Susu <tristan.daffe@gmail.com>
2023-02-13 15:28:26 +0100

    update keymap for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sw', builtin.live_grep, {})
