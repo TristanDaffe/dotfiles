bcbb2fc8750371dd508e549b35480bcd693b077c
Susu <100208156+TristanDaffe@users.noreply.github.com>
2023-02-13 13:11:15 +0100

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/cava/ ~/.config/cava
-ln -s ~/.dotfiles/kitty/ ~/.config/kitty
-ln -s ~/.dotfiles/neofetch/ ~/.config/neofetch
-ln -s ~/.dotfiles/nvim/ ~/.config/nvim
-ln -s ~/.dotfiles/rofi/ ~/.config/rofi
+ln -s ~/dotfiles/cava/ ~/.config/cava
+ln -s ~/dotfiles/kitty/ ~/.config/kitty
+ln -s ~/dotfiles/neofetch/ ~/.config/neofetch
+ln -s ~/dotfiles/nvim/ ~/.config/nvim
+ln -s ~/dotfiles/rofi/ ~/.config/rofi
