752a912c93a1a7d276a9e86f7c9a6befdac120c6
Susu <tristan.daffe@gmail.com>
2023-02-13 14:44:02 +0100

    config toggleterm

---
+++ nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,27 @@
+require"toggleterm".setup {
+  size = 13,
+  open_mapping = [[<c-t>]],
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = '1',
+  start_in_insert = true,
+  persist_size = true,
+  direction = 'horizontal',
+  hide_numbers = true,
+  persist_mode = true,
+  close_on_exit = true,
+  shell = vim.o.shell,
+  insert_mappings = true,
+}
+
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
