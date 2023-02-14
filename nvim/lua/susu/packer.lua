616e13b150cd166f7da7a96e0a6dbdbdd4eccd2c
Susu <tristan.daffe@gmail.com>
2023-02-14 12:19:59 +0100

    change to nvim

--- nvim/lua/susu/packer.lua
+++ nvim/lua/susu/packer.lua
@@ -5,7 +5,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use('wbthomason/packer.nvim')
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
--- nvim/lua/susu/remap.lua
+++ nvim/lua/susu/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set("n", "<leader>cs", ":nohl<CR>")
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<leader>m", ":Mason<CR>")
+
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
