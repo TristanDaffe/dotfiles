edab5fa0d0bc45d8ecac1dc985a562642cbd3d3d
Susu <tristan.daffe@gmail.com>
2023-04-26 12:13:19 +0200

    alias bashrc

---
+++ .bash_aliases
@@ -0,0 +1,7 @@
+# custom bash_aliases
+alias kill42='sudo kill $(sudo lsof -t -i:4200)'
+alias tfe='nvim /home/susu/Documents/Stage/TFE/Latex/'
+alias ll='ls -a'
+alias lazygit='git add -A && git commit -m "wip"'
+alias nv='nvim'
+alias vim='nvim'
--- nvim/lua/susu/remap.lua
+++ nvim/lua/susu/remap.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<F5>", ":setlocal spell! spelllang=fr<CR>")
