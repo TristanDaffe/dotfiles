b3055d57c748b3c3e71ec4e0c7e9b265db9dfa49
Susu <tristan.daffe@gmail.com>
2026-09-18 14:40:55 +0200

    chore: remove file path in nvim status line

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -53,7 +53,7 @@ local lualine_opts = {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
-        lualine_c = { { 'filename', path = 1 } },
+        lualine_c = { { 'filename'} },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
